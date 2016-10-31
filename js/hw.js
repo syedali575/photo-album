b037a5137f717099a4bf962e3415f8d14c9057e6
Syed Ali <sali3@cox.net>
2016-10-31 16:46:33 -0400

    Change code to remove toggle class; and use add and remove class instead to improve functionality

--- js/hw.js
+++ js/hw.js
@@ -8,23 +8,41 @@ console.log("I am in hw.js");
 $(".photos")
   .find("li")
       .on("click", function zooming(){
-        $(this).toggleClass("zoomed")
-        $(this).closest("main").toggleClass("zooming")
-        $(this).append($("nav"));
+        $(this).toggleClass("zoomed");
+        $("main").addClass("zooming");
+        $(".zoomed").append($("nav"));
 
   });
 
 $(".previous")
   .on("click", function previous(){
-    $(this).parent().parent().next("li")
-      .toggleClass("zoomed")
-      .append($("nav"));
+      // $(this).stopPropagation();
+
+    $(".zoomed").prev()
+      .addClass("zoomed");
+    $(".zoomed").prev()
+      .removeClass("zoomed");
+    $(".zoomed").append($("nav"));
+
+          // console.log($(this).parent().parent());
+          // console.log($(this).parent().parent().prev("li"));
+      // .closest("main").toggleClass("zooming")
+    // $(this).parent().parent().parent().parent().toggleClass("zooming")
+      // .append($("nav"));
+
     // this.parentNode.parentNode.parentNode.next("img");
-
-
 
   });
 
+  $(".next")
+    .on("click", function next(){
+      $(this).parent().parent().next("li")
+        .toggleClass("zoomed")
+        // .closest("main").toggleClass("zooming")
+        .append($("nav"));
+      // this.parentNode.parentNode.parentNode.next("img");
+
+    });
 
 
 
