6c67da6458348149627705607a7f9365a5538938
Syed Ali <sali3@cox.net>
2016-10-31 14:20:14 -0400

    Changed class of li and main and added nav element to li element

--- js/hw.js
+++ js/hw.js
@@ -5,7 +5,17 @@
 console.log("I am in hw.js");
 console.log($("main"));
 
+$(".photos")
+  .find("li")
+      .on("click", function zooming(){
+        $(this).toggleClass("zoomed")
+        $(this).closest("main").toggleClass("zooming");
+        $(this).append($("nav"));
 
+
+
+
+      })
 
 
 
