784134b7443a76a25557ea57edec25b043d42b1f
Syed Ali <sali3@cox.net>
2016-10-31 21:34:29 -0400

    Left and right buttons are working; I am unable to remove main element's class zooming' that is my last issue to resolve

--- js/hw.js
+++ js/hw.js
@@ -2,56 +2,37 @@
 
 "use strict";
 
-console.log("I am in hw.js");
-// console.log($("main"));
+// console.log("I am in hw.js");
+
 
 $(".photos")
   .find("li")
       .on("click", function zooming(){
         $(this).toggleClass("zoomed");
-        $("main").addClass("zooming");
+        $("main").toggleClass("zooming");
         $(".zoomed").append($("nav"));
 
   });
 
-$(".previous")
-  .on("click", function previous(){
-      // $(this).stopPropagation();
-
-    $(".zoomed").prev()
-      .addClass("zoomed");
-    $(".zoomed").prev()
-      .removeClass("zoomed");
-    $(".zoomed").append($("nav"));
-
-          // console.log($(this).parent().parent());
-          // console.log($(this).parent().parent().prev("li"));
-      // .closest("main").toggleClass("zooming")
-    // $(this).parent().parent().parent().parent().toggleClass("zooming")
-      // .append($("nav"));
-
-    // this.parentNode.parentNode.parentNode.next("img");
-
-  });
 
   $(".next")
     .on("click", function next(){
       $(this).parent().parent().next("li")
         .toggleClass("zoomed")
-        // .closest("main").toggleClass("zooming")
         .append($("nav"));
-      // this.parentNode.parentNode.parentNode.next("img");
+      $("main").removeClass();
 
     });
 
+    $(".previous")
+      .on("click", function next(){
+        $(this).parent().parent().prev("li")
+          .toggleClass("zoomed")
+          .append($("nav"));
+        $("main").removeClass();
 
 
-
-
-
-
-
-
+      });
 
 
 })();
