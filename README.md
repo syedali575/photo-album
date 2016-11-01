7a2f643a63b28a1c8c4d7ad31f9af8989125adb9
Syed Ali <sali3@cox.net>
2016-11-01 06:46:18 -0400

    Added description of project to read.me file; refactored code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # photo-album
+
+
+ In this assignment we are using jquery to dynamically add classes to element li and main; we are also appending nav element to li.
+
+  Result of above jquery implementation causes picture to be clicked: becomes zoomed in and navigation buttons appear on the picture to move next or previous.   
--- js/hw.js
+++ js/hw.js
@@ -2,36 +2,41 @@
 
 "use strict";
 
-// console.log("I am in hw.js");
 
-
+  // Choosing ul and then li.
+  // On click add classes to this li and main and also insert nav to li.
 $(".photos")
   .find("li")
       .on("click", function zooming(){
-        $(this).toggleClass("zoomed");
-        $("main").toggleClass("zooming");
-        $(".zoomed").append($("nav"));
+          $(this).toggleClass("zoomed");
+          $("main").toggleClass("zooming");
+          $(".zoomed").append($("nav"));
+      });
 
-  });
 
 
+// When next button of nav is clicked
+// Go up to li and move to next li
   $(".next")
     .on("click", function next(){
+      // Moving up to li
       $(this).parent().parent().next("li")
         .toggleClass("zoomed")
         .append($("nav"));
       $("main").removeClass();
-
     });
 
+
+
+// When previous button of nav is clicked
+// Go up to li and move to previous li
     $(".previous")
       .on("click", function next(){
+        // Moving up to li
         $(this).parent().parent().prev("li")
           .toggleClass("zoomed")
           .append($("nav"));
         $("main").removeClass();
-
-
       });
 
 
